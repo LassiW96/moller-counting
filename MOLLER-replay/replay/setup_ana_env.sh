c46c25b79a56487057194d02ee0679dee349eac5
lassi <lasithagayan9596@gmail.com>
2025-06-30 16:15:10 -0500

    analyzer files and db files updated

--- MOLLER-replay/replay/setup_ana_env.sh
+++ MOLLER-replay/replay/setup_ana_env.sh
@@ -4,9 +4,9 @@
 ## Use: source setup_ana_env.sh
 
 ## New environment for moller counting
-export MOLLER_REPLAY=$HOME/JLab-analyzers/moller-counting
+export MOLLER_REPLAY=$HOME/JLab-analyzers/moller-counting/MOLLER-replay
 
-export DB_DIR=$MOLLER_REPLAY/MOLLER-replay/DB
+export DB_DIR=$MOLLER_REPLAY/DB
 export DATA_DIR=$HOME/JLab-analyzers/moller-counting-test/data
 export ANALYZER_CONFIGPATH=$MOLLER_REPLAY/replay
 export OUT_DIR=$HOME/JLab-analyzers/moller-counting-test/output
