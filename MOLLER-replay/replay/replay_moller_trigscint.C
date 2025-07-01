fa29aaea7647c01d27d7bd380c50a27ed8bd3153
lassi <lasithagayan9596@gmail.com>
2025-07-01 16:36:44 -0500

    replay_moller_trigscint script updated

--- MOLLER-replay/replay/replay_moller_trigscint.C
+++
@@ -1,8 +0,0 @@
-/* Replay script for analyzing moller trigger scintillator data */
-#include "TSystem.h"
-#include "TList.h"
-#include "THaRun.h"
-#include "THaEvent.h"
-#include "THaAnalyzer.h"
-#include "THaApparatus.h"
-#include "TString.h"
